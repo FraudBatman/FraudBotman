c301dd66b6374a1eaadb28db69fb9453c198747c
Nic Ott <fraud@nics-mbp.home>
2018-07-18 00:33:13 -0400

    Added basic discord functionality

--- src/discord/Program.cs
+++ src/discord/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace DiscordQuiplash
 {
     class Program
     {
-        static void Main(string[] args)
+        /*MEMBERS*/
+        DiscordSocketClient client = null;
+        string token = "";
+
+        /*FUNCTIONS*/
+
+        /// <summary>
+        /// Main function. Used to start the program asynchronus
+        /// </summary>
+        public static void Main(string[] args)
+                    => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            client = new DiscordSocketClient();
+
+            //log into discord
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            /*ACTIONS*/
+            client.Log += Log;
+
+            await Task.Delay(-1);
+        }
+
+        async Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            await Task.CompletedTask;
         }
     }
 }
